a0408ed1c2a30fb79bac1d13e7b01fbd54422587
PIFSS <zainabkram11@gmail.com>
2017-03-30 15:12:16 +0300

    Update

--- app/src/main/java/com/example/mhd/donor/MakeDonations.java
+++ app/src/main/java/com/example/mhd/donor/MakeDonations.java
@@ -54,27 +54,24 @@ public class MakeDonations extends Fragment {
         InstructionForDonation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getActivity(),StaticInfoFragment.class);
+                Intent i = new Intent(getActivity(), StaticInfoFragment.class);
                 startActivity(i);
             }
         });
 
         Spinner donationType = (Spinner) vv.findViewById(R.id.spinnerSelectDonationType);
-        final ArrayList<String> model=new ArrayList<>();
+        final ArrayList<String> model = new ArrayList<>();
 
         model.add("Select Donation Type");
         model.add("Blood Cells");
         model.add("Platelets");
 
-        final ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,model);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, model);
         donationType.setAdapter(adapter);
 
 
-
-
-
         Spinner branchType = (Spinner) vv.findViewById(R.id.spinnerBranches);
-        final ArrayList<String> model1=new ArrayList<>();
+        final ArrayList<String> model1 = new ArrayList<>();
 
         model1.add("Select Branch");
         model1.add("AlJabriya- Main Branch");
@@ -82,7 +79,7 @@ public class MakeDonations extends Fragment {
         model1.add("AlJahra'a");
         model1.add("AlAsma");
 
-         final ArrayAdapter<String> adapterr =new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,model1);
+        final ArrayAdapter<String> adapterr = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, model1);
 
         branchType.setAdapter(adapterr);
 
@@ -104,7 +101,6 @@ public class MakeDonations extends Fragment {
             public void onClick(final View v) {
 
 
-
                 DatePickerDialog d = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -112,7 +108,6 @@ public class MakeDonations extends Fragment {
                         Toast.makeText(getActivity(), year + "/" + month + "/" + dayOfMonth, Toast.LENGTH_SHORT).show();
 
                         date.setText(year + "/" + month + "/" + dayOfMonth);
-
 
 
                     }
@@ -130,12 +125,10 @@ public class MakeDonations extends Fragment {
             public void onClick(final View v) {
 
 
-
-
                 TimePickerDialog d = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        time.setText(hourOfDay+":"+minute);
+                        time.setText(hourOfDay + ":" + minute);
                     }
                 }, 12, 0, false);
 
@@ -144,21 +137,9 @@ public class MakeDonations extends Fragment {
         });
 
 
-        Button confirm =(Button) vv.findViewById(R.id.confirmButton);
-        confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-            Toast
-
-                startActivity(i);
-            }
-        });
-
-        });
         return vv;
 
     }
+}
 
 
