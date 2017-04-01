0ac4e6be0ae7cea4835546c1fc02d042680f7c2d
PIFSS <zainabkram11@gmail.com>
2017-04-01 18:31:08 +0300

    Update

--- app/src/main/java/com/example/mhd/donor/Settings.java
+++ app/src/main/java/com/example/mhd/donor/Settings.java
@@ -1,11 +1,19 @@
 package com.example.mhd.donor;
 
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import java.util.Locale;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 /**
@@ -13,7 +21,10 @@ import android.view.ViewGroup;
  */
 public class Settings extends Fragment {
 
-
+    Button english;
+    Button arabic;
+    Resources resEnglish;
+    Resources resArabic;
     public Settings() {
         // Required empty public constructor
     }
@@ -22,8 +33,44 @@ public class Settings extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        english = (Button)  v.findViewById(R.id.button2);
+        arabic= (Button) v.findViewById(R.id.button4);
+
+        english.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Locale locale = new Locale("en");
+               locale.setDefault(locale);
+                resEnglish = getResources();
+                DisplayMetrics dm = resEnglish.getDisplayMetrics();
+                Configuration conf =resEnglish.getConfiguration();
+                conf.locale = locale;
+                resEnglish.updateConfiguration(conf, dm);
+
+
+
+
+            }
+        });
+
+        arabic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Locale locale = new Locale("ar");
+                locale.setDefault(locale);
+                resArabic = getResources();
+                DisplayMetrics dm1 = resArabic.getDisplayMetrics();
+                Configuration conf1 = resArabic.getConfiguration();
+                conf1.locale = locale;
+                resArabic.updateConfiguration(conf1, dm1);
+
+            }
+        });
+
+        return v;
     }
+
 
 }
